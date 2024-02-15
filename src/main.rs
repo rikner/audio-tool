e1fa376cfdfcdc3a04d3a1d77cfc6e40d934079f
Erik Werner <rikner@web.de>
2024-02-16 00:01:07 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+mod tools;
+
 use circular_queue::CircularQueue;
 
+use tools::*;
 use nannou::prelude::*;
 use nannou_audio as audio;
 use pitch_detection::detector::mcleod::McLeodDetector;
@@ -8,18 +11,26 @@ use pitch_detection::detector::PitchDetector;
 use pitch_detection::Pitch;
 use std::sync::{Arc, Mutex};
 
+use spectrum_analyzer::{samples_fft_to_spectrum, FrequencyLimit};
+use spectrum_analyzer::windows::hann_window;
+use spectrum_analyzer::scaling::divide_by_N_sqrt;
+
+
 type AudioQueue = Arc<Mutex<CircularQueue<f32>>>;
 
-const BUFFER_LEN_FRAMES: usize = 512;
-const POWER_THRESHOLD: f32 = 2.0;
-const CLARITY_THRESHOLD: f32 = 0.3;
+const BUFFER_LEN_FRAMES: usize = 2048;
+const POWER_THRESHOLD: f32 = 0.1;
+const CLARITY_THRESHOLD: f32 = 0.01;
 
 struct Model {
     queue: AudioQueue,
     _stream: audio::Stream<AudioQueue>, // keeps stream alive
-    detector: YINDetector<f32>,
+    detector: McLeodDetector<f32>,
     pitch: Option<Pitch<f32>>,
+    musical_note: String,
+    deviation: f32,
     sample_rate: u32,
+    power_spectrum: Vec<f32>,
 }
 
 fn main() {
@@ -44,14 +55,17 @@ fn model(_app: &App) -> Model {
 
     let padding: usize = BUFFER_LEN_FRAMES / 2;
 
-    let detector: YINDetector<f32> = YINDetector::new(BUFFER_LEN_FRAMES, padding);
+    let detector: McLeodDetector<f32> = McLeodDetector::new(BUFFER_LEN_FRAMES, padding);
 
     Model {
         detector,
         queue,
         _stream: stream,
         pitch: None,
+        musical_note: "".to_string(),
+        deviation: 0.0,
         sample_rate,
+        power_spectrum: vec![0.0; BUFFER_LEN_FRAMES],
     }
 }
 
@@ -66,7 +80,6 @@ fn update(_app: &App, model: &mut Model, _update: Update) {
     let binding = model.queue.lock().unwrap();
     let current_audio_data = binding.iter().collect::<Vec<_>>();
 
-    // copy current audio data into a Vec<f32>
     let signal = current_audio_data
         .iter()
         .map(|&x| x.to_owned())
@@ -79,7 +92,34 @@ fn update(_app: &App, model: &mut Model, _update: Update) {
         CLARITY_THRESHOLD,
     );
 
-    model.pitch = pitch
+    let note_and_deviation = match pitch.as_ref() {
+        Some(pitch) => frequency_to_note_with_deviation(pitch.frequency),
+        None => NoteAndDeviation {
+            musical_note: "".to_string(),
+            deviation: 0.0,
+        },
+    };
+
+    let hann_window = hann_window(&signal);
+    // calc spectrum
+    let spectrum_hann_window = samples_fft_to_spectrum(
+        // (windowed) samples
+        &hann_window,
+        // sampling rate
+        44100,
+        // optional frequency limit: e.g. only interested in frequencies 50 <= f <= 150?
+        FrequencyLimit::All,
+        // optional scale
+        Some(&divide_by_N_sqrt),
+    ).unwrap();
+
+    let power_spectrum = spectrum_hann_window.data().iter().map(|(_, x)| x.val().abs().powf(2.0)).collect::<Vec<f32>>();
+
+    model.pitch = pitch;
+    model.musical_note = note_and_deviation.musical_note;
+    model.deviation = note_and_deviation.deviation;
+    model.power_spectrum = power_spectrum;
+    
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
@@ -90,15 +130,22 @@ fn view(app: &App, model: &Model, frame: Frame) {
     let current_audio_data = binding.iter().collect::<Vec<_>>();
 
     draw.background().rgb(0.07, 0.09, 0.15);
-    draw_pitch(&draw, boundary, &model.pitch);
-    draw_wav_form(&draw, boundary, current_audio_data);
+    draw_pitch_and_deviation(&draw, boundary, &model.pitch, model.deviation);
+    // draw_wav_form(&draw, boundary, current_audio_data);
+    draw_power_spectrum(&draw, boundary, &model.power_spectrum);
     draw.to_frame(app, &frame).unwrap();
 }
 
 fn draw_wav_form(draw: &Draw, boundary: geom::Rect, audio_data: Vec<&f32>) {
     // draw each point in the vector to create a wav form
     for (i, sample) in audio_data.iter().enumerate() {
-        let x = map_range(i, 0, audio_data.len(), boundary.left(), boundary.right());
+        let x = map_range(
+            i,
+            0,
+            audio_data.len(),
+            boundary.left(),
+            boundary.right()
+        );
 
         draw.rect()
             .rgb(0.95, 0.55, 0.25)
@@ -107,14 +154,52 @@ fn draw_wav_form(draw: &Draw, boundary: geom::Rect, audio_data: Vec<&f32>) {
     }
 }
 
-fn draw_pitch(draw: &Draw, boundary: geom::Rect, pitch: &Option<Pitch<f32>>) {
+
+fn draw_pitch_and_deviation(draw: &Draw, boundary: geom::Rect, pitch: &Option<Pitch<f32>>, deviation: f32) {
     let text = match pitch {
-        Some(pitch) => format!("{:.2} Hz", pitch.frequency),
-        None => "No pitch detected".to_string(),
+        Some(pitch) => frequency_to_note(pitch.frequency),
+        None => "".to_string(),
     };
 
     draw.text(&text)
         .rgb(0.95, 0.55, 0.25)
         .font_size(24)
         .y(boundary.top() - 50.0);
+
+    // let deviation_text = format!("Deviation: {:.2}", deviation);
+    // draw.text(&deviation_text)
+    //     .rgb(0.95, 0.55, 0.25)
+    //     .font_size(24)
+    //     .y(boundary.top() - 80.0);
+
+    // draw a meter below the pitch text in the center to show the deviation from the note
+    let x_centered = boundary.x();
+    let w = 100.0;
+    let h = 20.0;
+    let y = boundary.top() - 100.0;
+    let deviation_meter_rect = geom::Rect::from_x_y_w_h(x_centered, y, w, h);
+ 
+    // draw.rect()
+    //     .color(rgba(0.95, 0.55, 0.25, 0.5))
+    //     .stroke(rgba(0.95, 0.55, 0.25, 0.5))
+    //     .stroke_weight(1.0)
+    //     .x_y(deviation_meter_rect.x(), deviation_meter_rect.y())
+    //     .w_h(deviation_meter_rect.w(), deviation_meter_rect.h());
+
+    let deviation_meter_needle_x = map_range(
+        deviation,
+        -100.0,
+        100.0,
+        deviation_meter_rect.left(),
+        deviation_meter_rect.right(),
+    );
+
+    // draw.rect()
+    //     .color(rgba(0.95, 0.55, 0.25, 0.9))
+    //     .stroke(GREEN)
+    //     .stroke_weight(1.0)
+    //     .x_y(deviation_meter_needle_x, deviation_meter_rect.y())
+    //     .w_h(2.0, deviation_meter_rect.h());
+    
+
 }
---
+++ src/tools.rs
@@ -0,0 +1,29 @@
+
+const MUSICAL_NOTES: [&str; 12] = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+
+pub fn frequency_to_note(freq: f32) -> String {
+    let note: i32 = (12.0 * (freq / 440.0).log2()).round() as i32;
+    let note_index = (note + 69) % 12;
+    MUSICAL_NOTES[note_index as usize].to_string()
+}
+
+pub struct NoteAndDeviation {
+    pub musical_note: String,
+    pub deviation: f32,
+}
+
+pub fn frequency_to_note_with_deviation(freq: f32) -> NoteAndDeviation {
+    let musical_note = frequency_to_note(freq);
+    let note_frequency = note_to_frequency(&musical_note);
+    let deviation = (freq / note_frequency).log2();
+
+    NoteAndDeviation {
+        musical_note,
+        deviation,
+    }
+}
+
+pub fn note_to_frequency(note: &str) -> f32 {
+    let note_index = MUSICAL_NOTES.iter().position(|&x| x == note).unwrap() as i32;
+    440.0 * 2.0_f32.powf((note_index - 9) as f32 / 12.0)
+}
