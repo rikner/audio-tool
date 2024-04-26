3e3b42490598fac9115f31e910911fb8cbbd5779
rikner <rikner@web.de>
2024-04-26 12:51:24 +0200

    use Arc and Mutex

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
-use nannou::prelude::*;
+use nannou::{color::chromatic_adaptation::AdaptInto, prelude::*};
 use std::time::SystemTime;
 use circular_queue::CircularQueue;
+use std::sync::{Arc, Mutex};
 
 // https://docs.rs/nannou_audio/latest/nannou_audio/
 use nannou_audio as audio;
 
+type AudioQueue = Arc<Mutex<CircularQueue<f32>>>;
+
 struct StreamModel {
-	audio_circular_queue: CircularQueue<f32>,
+	audio_queue: AudioQueue,
 }
 
 struct Model {
-	audio_circular_queue: CircularQueue<f32>,
+	audio_queue: AudioQueue,
     stream: audio::Stream<StreamModel>,
 }
 
@@ -20,17 +23,19 @@ fn main() {
 
 fn on_audio_data(model: &mut StreamModel, buffer: &audio::Buffer) {
 	println!("updating audio data at time: {:?}", SystemTime::now());
+	let mut queue = model.audio_queue.lock().unwrap();
 	buffer.frames().for_each(|frame| {
 		frame.iter().for_each(|sample| {
-			model.audio_circular_queue.push(*sample);
+			let sample_value: f32 = *sample;
+			queue.push(sample_value);
 		});
 	});
 }
 
 fn model(_app: &App) -> Model {
-	let audio_circular_queue = CircularQueue::<f32>::with_capacity(8192);
+	let audio_queue = Arc::new(Mutex::new(CircularQueue::<f32>::with_capacity(8192)));
     let stream_model = StreamModel {
-    	audio_circular_queue: audio_circular_queue,
+    	audio_queue: audio_queue.clone(),
     };
     let audio_host = audio::Host::new();
     let input_stream = audio_host
@@ -42,7 +47,7 @@ fn model(_app: &App) -> Model {
 
     Model {
         stream: input_stream,
-        audio_circular_queue: audio_circular_queue,
+        audio_queue,
     }
 }
 
@@ -53,7 +58,23 @@ fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     let boundary = app.window_rect();
 
-    draw.text("Hello World!").font_size(48).color(ORANGE);
+    draw.background().rgb(0.07, 0.09, 0.15);
+
+    let binding = model.audio_queue.lock().unwrap();
+    let current_audio_data = binding.iter().collect::<Vec<_>>();
+    draw_wav_form(&draw, boundary, current_audio_data);
 
     draw.to_frame(app, &frame).unwrap();
+}
+
+fn draw_wav_form(draw: &Draw, boundary: geom::Rect, audio_data: Vec<&f32>) {
+    // draw each point in the vector to create a wav form
+    for (i, sample) in audio_data.iter().enumerate() {
+        let x = map_range(i, 0, audio_data.len(), boundary.left(), boundary.right());
+
+        draw.rect()
+            .rgb(0.95, 0.55, 0.25)
+            .x_y(x, 0.0)
+            .w_h(1.0, *sample * 1000.0);
+    }
 }
